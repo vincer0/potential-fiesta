a8794e89b1f05d4d3f4000a26b11c1c327bf298d
adam <dev@apdev.pl>
2026-01-20 13:07:27 +0100

    actually make live components to modify store

--- src/components/live-starter.tsx
+++ src/components/live-starter.tsx
@@ -1,10 +1,40 @@
-'use client';
+"use client";
 
-import { startLiveOddsUpdates } from "@/live/live-odds-gate";
+import { useEffect, useRef } from "react";
+import { useAppStore } from "@/providers/store-provider";
 
 const LiveStarter = () => {
-    startLiveOddsUpdates();
-    return null;
-}
+  const oucomes = useAppStore((state) => state.eventGameOutcomes);
+  const updateOutcomeOdds = useAppStore((state) => state.updateOutcomeOdds);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      const keys = Object.keys(oucomes);
+      if (!keys.length) return;
+
+      const randomIndex = Math.floor(Math.random() * keys.length);
+      const id = Number(keys[0]); // TODO: hadrdcoded for testing purposes
+      const outcome = oucomes[id];
+      if (!outcome) return;
+
+      const multiplier = Math.random() * (1.1 - 0.9) + 0.9;
+      const newOdds = Number((outcome.outcomeOdds * multiplier).toFixed(2));
+
+      updateOutcomeOdds(id, newOdds);
+    }, 1000);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, [updateOutcomeOdds, oucomes]);
+
+  return null;
+};
 
 export default LiveStarter;

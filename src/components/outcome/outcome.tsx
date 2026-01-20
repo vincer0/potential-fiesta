9a47e3e136024336771794b66cf5af6679a85161
adam <dev@apdev.pl>
2026-01-20 13:06:58 +0100

    get rid of live module

--- src/components/outcome/outcome.tsx
+++ src/components/outcome/outcome.tsx
@@ -2,12 +2,6 @@
 import clsx from "clsx";
 import { useAppStore } from "@/providers/store-provider";
 import { type Outcome } from "@/types/outcome";
-import { useEffect } from "react";
-import {
-  LiveOddsUpdate,
-  subscribeToLiveOdds,
-  unsubscribeFromLiveOdds,
-} from "@/live/live-odds-gate";
 
 interface OutcomeProps {
   outcomeId: number;
@@ -19,27 +13,6 @@ const Outcome = ({ outcomeId, eventId, eventGameId }: OutcomeProps) => {
   const { outcomeOdds, isSelected, outcomeName } = useAppStore((state) => state.eventGameOutcomes[outcomeId]);
   const addToBetslip = useAppStore((state) => state.addToBetslip);
   const removeFromBetslip = useAppStore((state) => state.removeFromBetslip);
-  const updateOutcomeOdds = useAppStore((state) => state.updateOutcomeOdds);
-
-  useEffect(() => {
-    const subId = subscribeToLiveOdds((update: LiveOddsUpdate) => {
-      if (
-        update.outcomeId === outcomeId &&
-        update.eventId === eventId &&
-        update.eventGameId === eventGameId
-      ) {
-        console.log("Received live odds update:", update);
-        updateOutcomeOdds(
-          outcomeId,
-          update.newOdds,
-        );
-      }
-    });
-
-    return () => {
-      unsubscribeFromLiveOdds(subId);
-    };
-  }, [outcomeId, eventId, eventGameId, updateOutcomeOdds]);
 
   const handleOnClick = () => {
     if (isSelected) {
